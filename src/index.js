b615df8510c2617031186ea1773f10fb82d298fd
Yingfan <1874375591@qq.com>
2022-09-30 05:15:46 +0000

    Update 7 files

--- src/index.js
+++ src/index.js
@@ -15,7 +15,28 @@ document.getElementById("app").innerHTML = `
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  <a href="https://parceljs.org" target="_blan
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel k" rel="noopener noreferrer">here</a>.
 </div>
 `;
 import "./styles.css";
